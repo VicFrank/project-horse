4e3515a6d4de3a2bd40b2868ba2586342bb4a11c
VicFrank <matthewshattil@gmail.com>
2022-02-20 23:55:42 -0600

    fixed import

--- routes/battlepass.js
+++ routes/battlepass.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const battlePasses = require("../db/battlePass");
+const battlePasses = require("../db/battlepass");
 
 router.get("/", async (req, res) => {
   try {
