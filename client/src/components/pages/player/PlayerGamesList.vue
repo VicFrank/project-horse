952b45f0b3d4858290d49ed06f15d3818b970b67
VicFrank <matthewshattil@gmail.com>
2022-04-08 21:44:38 -0500

    changed mmr

--- client/src/components/pages/player/PlayerGamesList.vue
+++ client/src/components/pages/player/PlayerGamesList.vue
@@ -1,51 +1,47 @@
 <template>
-  <div>
-    <h2 class="page-title">My Games</h2>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>God</th>
-          <th>Result</th>
-          <th>Rounds</th>
-          <th>Ranked</th>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>God</th>
+        <th>Result</th>
+        <th>Rounds</th>
+        <th>Ranked</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <template v-if="loading">
+        <tr v-for="i in placeholderRows" :key="i" class="animate-pulse">
+          <td />
         </tr>
-      </thead>
-
-      <tbody>
-        <template v-if="loading">
-          <tr v-for="i in placeholderRows" :key="i" class="animate-pulse">
-            <td />
-          </tr>
-        </template>
-        <template v-else>
-          <router-link
-            v-for="game in games"
-            :key="game.game_id"
-            :to="'/games/' + game.game_id"
-            tag="tr"
-          >
-            <td>{{ game.god }}</td>
-            <td>
-              {{ getRankString(game.place) }}
-              <template v-if="game.mmr_change != undefined">
-                <span v-if="game.mmr_change >= 0" class="win">
-                  +{{ game.mmr_change }}
-                </span>
-                <span v-else class="loss"> -{{ game.mmr_change }} </span>
-              </template>
-              <div class="text-muted">{{ dateFromNow(game.created_at) }}</div>
-            </td>
-            <td>
-              {{ game.rounds || "?" }} Rounds
-
-              <div class="text-muted">{{ hhmmss(game.duration) }}</div>
-            </td>
-            <td>{{ game.ranked ? "Ranked" : "Unranked" }}</td>
-          </router-link>
-        </template>
-      </tbody>
-    </table>
-  </div>
+      </template>
+      <template v-else>
+        <router-link
+          v-for="game in games"
+          :key="game.game_id"
+          :to="'/games/' + game.game_id"
+          tag="tr"
+        >
+          <td>{{ game.god }}</td>
+          <td>
+            {{ getRankString(game.place) }}
+            <template v-if="game.mmr_change != undefined">
+              <span v-if="game.mmr_change >= 0" class="win">
+                +{{ game.mmr_change }}
+              </span>
+              <span v-else class="loss"> {{ game.mmr_change }} </span>
+            </template>
+            <div class="text-muted">{{ dateFromNow(game.created_at) }}</div>
+          </td>
+          <td>
+            {{ game.rounds || "?" }} Rounds
+            <div class="text-muted">{{ hhmmss(game.duration) }}</div>
+          </td>
+          <td>{{ game.ranked ? "Ranked" : "Unranked" }}</td>
+        </router-link>
+      </template>
+    </tbody>
+  </table>
 </template>
  
 <script>
