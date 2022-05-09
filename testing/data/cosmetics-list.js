da25b31d2b8c4b1d4654cfc7652145d94792df72
VicFrank <matthewshattil@gmail.com>
2022-05-08 22:23:03 -0500

    ported images from dota

--- testing/data/cosmetics-list.js
+++ testing/data/cosmetics-list.js
@@ -1,46 +1,405 @@
 module.exports = [
+  /**
+   * *************************
+   * Avatar Items
+   * *************************
+   */
   {
-    name: "avatar_bronze",
-    type: "Avatar",
-    equip_group: "avatar",
+    // Battle Pass Reward
+    name: "avatar_gaben",
+    type: "Avatar Frame",
+    equip_group: "avatar_frame",
     coins: -1,
     cost_usd: -1,
-    rarity: "",
+    rarity: "Ancient",
   },
   {
-    name: "avatar_silver",
-    type: "Avatar",
-    equip_group: "avatar",
+    // Battle Pass Reward
+    name: "avatar_gambler_diamond",
+    type: "Avatar Frame",
+    equip_group: "avatar_frame",
     coins: -1,
     cost_usd: -1,
-    rarity: "",
+    rarity: "Arcana",
   },
   {
-    name: "avatar_gold",
-    type: "Avatar",
-    equip_group: "avatar",
+    // Chest drop
+    name: "avatar_red",
+    type: "Avatar Frame",
+    equip_group: "avatar_frame",
     coins: -1,
     cost_usd: -1,
-    rarity: "",
+    rarity: "Common",
   },
   {
+    // Chest drop
+    name: "avatar_blue",
+    type: "Avatar Frame",
+    equip_group: "avatar_frame",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Common",
+  },
+  {
+    // Chest drop
+    name: "avatar_green",
+    type: "Avatar Frame",
+    equip_group: "avatar_frame",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Common",
+  },
+  {
+    // Battle Pass Reward
     name: "avatar_gambler",
-    type: "Avatar",
-    equip_group: "avatar",
+    type: "Avatar Frame",
+    equip_group: "avatar_frame",
     coins: -1,
     cost_usd: -1,
-    rarity: "",
+    rarity: "Immortal",
   },
   {
+    // Battle Pass Reward
+    name: "avatar_gold",
+    type: "Avatar Frame",
+    equip_group: "avatar_frame",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Legendary",
+  },
+  {
+    // Battle Pass Reward
+    name: "avatar_silver",
+    type: "Avatar Frame",
+    equip_group: "avatar_frame",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Mythical",
+  },
+  {
+    // Chest Drop
+    name: "avatar_pink",
+    type: "Avatar Frame",
+    equip_group: "avatar_frame",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "avatar_black",
+    type: "Avatar Frame",
+    equip_group: "avatar_frame",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Battle Pass Reward
+    name: "avatar_bronze",
+    type: "Avatar Frame",
+    equip_group: "avatar_frame",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_poo",
+    type: "Avatar Emote",
+    equip_group: "avatar_emote",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_horse",
+    type: "Avatar Emote",
+    equip_group: "avatar_emote",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_tea",
+    type: "Avatar Emote",
+    equip_group: "avatar_emote",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_beer",
+    type: "Avatar Emote",
+    equip_group: "avatar_emote",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_smiley",
+    type: "Avatar Emote",
+    equip_group: "avatar_emote",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_sunglasses",
+    type: "Avatar Eyewear",
+    equip_group: "avatar_eyewear",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_monacle",
+    type: "Avatar Eyewear",
+    equip_group: "avatar_eyewear",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_elton",
+    type: "Avatar Eyewear",
+    equip_group: "avatar_eyewear",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_patch",
+    type: "Avatar Eyewear",
+    equip_group: "avatar_eyewear",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_googly",
+    type: "Avatar Eyewear",
+    equip_group: "avatar_eyewear",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_angry",
+    type: "Avatar Eyewear",
+    equip_group: "avatar_eyewear",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_handlebar",
+    type: "Avatar Facial Hair",
+    equip_group: "avatar_facial_hair",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_fullbeard",
+    type: "Avatar Facial Hair",
+    equip_group: "avatar_facial_hair",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_french",
+    type: "Avatar Facial Hair",
+    equip_group: "avatar_facial_hair",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_goatee",
+    type: "Avatar Facial Hair",
+    equip_group: "avatar_facial_hair",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_tophat",
+    type: "Avatar Facial Hair",
+    equip_group: "avatar_facial_hair",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_santa",
+    type: "Avatar Hat",
+    equip_group: "avatar_hat",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_cowboy",
+    type: "Avatar Hat",
+    equip_group: "avatar_hat",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_beret",
+    type: "Avatar Hat",
+    equip_group: "avatar_hat",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_cap",
+    type: "Avatar Hat",
+    equip_group: "avatar_hat",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_wizardhat",
+    type: "Avatar Hat",
+    equip_group: "avatar_hat",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Leaderboard Reward
+    name: "hat_wizardhat",
+    type: "Avatar Hat (Leaderboard)",
+    equip_group: "avatar_hat",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_lips",
+    type: "Avatar Mouth",
+    equip_group: "avatar_mouth",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_pipe",
+    type: "Avatar Mouth",
+    equip_group: "avatar_mouth",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_cigar",
+    type: "Avatar Mouth",
+    equip_group: "avatar_mouth",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_gas",
+    type: "Avatar Mouth",
+    equip_group: "avatar_mouth",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_covid",
+    type: "Avatar Mouth",
+    equip_group: "avatar_mouth",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "hat_tongue",
+    type: "Avatar Mouth",
+    equip_group: "avatar_mouth",
+    coins: -1,
+    cost_usd: -1,
+    rarity: "Rare",
+  },
+  /**
+   * *************************
+   * Consumables
+   * *************************
+   */
+  {
+    // Real Money Shop Purchase
+    name: "buy_bp",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: 6.99,
+    equip_group: "",
+    rarity: "Rare",
+  },
+  {
+    // Battle Pass Reward
     name: "double_down",
     type: "Consumable",
     coins: -1,
     cost_usd: -1,
     equip_group: "",
-    rarity: "Common",
+    rarity: "Rare",
   },
   {
-    name: "plus_booster1",
+    // Plus Reward
+    name: "plus_double_down",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "",
+    rarity: "Rare",
+  },
+  {
+    // Real Money Shop Purchase
+    // Purchase comes with two god chests
+    name: "plus_year_package",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: 55,
+    equip_group: "",
+    rarity: "Legendary",
+  },
+  {
+    // Chest Drop
+    name: "plus_1day",
     type: "Consumable",
     coins: -1,
     cost_usd: -1,
@@ -48,7 +407,260 @@ module.exports = [
     rarity: "Common",
   },
   {
-    name: "finisher_fx_gambler",
+    // Chest Drop
+    name: "plus_2day",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "",
+    rarity: "Uncommon",
+  },
+  {
+    // Chest Drop
+    name: "plus_4day",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "",
+    rarity: "Rare",
+  },
+  {
+    // Real Money Shop Purchase
+    name: "plus_month",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: 5,
+    equip_group: "",
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "get_xp_250",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "",
+    rarity: "Common",
+  },
+  {
+    // Chest Drop
+    name: "get_xp_500",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "",
+    rarity: "Uncommon",
+  },
+  {
+    // Chest Drop
+    name: "get_xp_1000",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "",
+    rarity: "Rare",
+  },
+  {
+    // Chest Drop
+    name: "get_xp_2500",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "",
+    rarity: "Mythical",
+  },
+  {
+    // Chest Drop
+    name: "get_xp_10k",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "",
+    rarity: "Legendary",
+  },
+  {
+    // Real Money Purchase
+    name: "buy_xp_5000",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: 24.99,
+    equip_group: "",
+    rarity: "Legendary",
+  },
+  {
+    // Real Money Purchase
+    name: "buy_xp_1000",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: 5.99,
+    equip_group: "",
+    rarity: "Mythical",
+  },
+  {
+    // Real Money Purchase
+    name: "buy_xp_300",
+    type: "Consumable",
+    coins: -1,
+    cost_usd: 1.99,
+    equip_group: "",
+    rarity: "Rare",
+  },
+  /**
+   * *************************
+   * Card Frames (From God Chest)
+   * *************************
+   */
+  {
+    name: "card_default",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Common",
+  },
+  {
+    name: "card_pudge",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_dazzle",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_shopkeeper",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_brewmaster",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_anshu",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_nimbus",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_legion",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_aghanim",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_lifestealer",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_kanna",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_ogre",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_rix",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_gambler",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_pa",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_alchemist",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_rune",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  {
+    name: "card_sorla",
+    type: "Card Frame",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "card_frame",
+    rarity: "Immortal",
+  },
+  /**
+   * *************************
+   * Finishers
+   * *************************
+   */
+  {
+    name: "finisher_default",
     type: "Finisher",
     coins: -1,
     cost_usd: -1,
@@ -56,13 +668,161 @@ module.exports = [
     rarity: "Common",
   },
   {
-    name: "arena_gambler",
-    type: "Arena",
+    name: "finisher_fire",
+    type: "Finisher",
     coins: -1,
     cost_usd: -1,
-    equip_group: "arena",
+    equip_group: "finisher",
     rarity: "Common",
   },
+  {
+    name: "finisher_lightning",
+    type: "Finisher",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "finisher",
+    rarity: "Common",
+  },
+  {
+    name: "finisher_poison",
+    type: "Finisher",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "finisher",
+    rarity: "Common",
+  },
+  {
+    name: "finisher_rocks",
+    type: "Finisher",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "finisher",
+    rarity: "Common",
+  },
+  {
+    name: "finisher_snow",
+    type: "Finisher",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "finisher",
+    rarity: "Common",
+  },
+  {
+    name: "finisher_god_gambler",
+    type: "Finisher",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "finisher",
+    rarity: "Immortal",
+  },
+  {
+    name: "finisher_god_pa",
+    type: "Finisher",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "finisher",
+    rarity: "Immortal",
+  },
+  {
+    name: "finisher_god_rune",
+    type: "Finisher",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "finisher",
+    rarity: "Immortal",
+  },
+  /**
+   * *************************
+   * Terrains
+   * *************************
+   */
+  {
+    name: "terrain_default",
+    type: "Terrain",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "terrain",
+    rarity: "Common",
+  },
+  {
+    name: "terrain_green",
+    type: "Terrain",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "terrain",
+    rarity: "Common",
+  },
+  {
+    name: "terrain_icelake",
+    type: "Terrain",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "terrain",
+    rarity: "Common",
+  },
+  {
+    name: "terrain_lava",
+    type: "Terrain",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "terrain",
+    rarity: "Common",
+  },
+  {
+    name: "terrain_snow",
+    type: "Terrain",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "terrain",
+    rarity: "Common",
+  },
+  {
+    name: "terrain_tropical",
+    type: "Terrain",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "terrain",
+    rarity: "Common",
+  },
+  {
+    name: "terrain_god_pa",
+    type: "Terrain",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "terrain",
+    rarity: "Immortal",
+  },
+  {
+    name: "terrain_god_rune",
+    type: "Terrain",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "terrain",
+    rarity: "Immortal",
+  },
+  {
+    // For getting top 1 in leaderboard or tournament winner
+    name: "terrain_champion",
+    type: "Terrain",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "terrain",
+    rarity: "Immortal",
+  },
+  {
+    // For getting top 25 in monthly leaderboard
+    name: "terrain_leaderboard",
+    type: "Terrain",
+    coins: -1,
+    cost_usd: -1,
+    equip_group: "terrain",
+    rarity: "Immortal",
+  },
+  /**
+   * *************************
+   * Chests
+   * *************************
+   */
   {
     name: "chest_basic",
     type: "Chest",
@@ -72,27 +832,20 @@ module.exports = [
     rarity: "Common",
   },
   {
+    // Battle Pass Reward
     name: "chest_gold",
     type: "Chest",
     coins: -1,
     cost_usd: -1,
     equip_group: "",
-    rarity: "Uncommon",
+    rarity: "common",
   },
   {
     name: "chest_god",
     type: "Chest",
-    coins: -1,
+    coins: 10000,
     cost_usd: -1,
     equip_group: "",
-    rarity: "Rare",
-  },
-  {
-    name: "god_gambler",
-    type: "God",
-    coins: -1,
-    cost_usd: -1,
-    equip_group: "",
-    rarity: "Rare",
+    rarity: "Immortal",
   },
 ];
