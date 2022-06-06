f00742db05765ae9d607c765cdc48e8e96d9c1d5
VicFrank <matthewshattil@gmail.com>
2022-06-05 21:53:09 -0500

    updated player stats pages

--- client/src/components/pages/player/PlayerPage.vue
+++ client/src/components/pages/player/PlayerPage.vue
@@ -2,13 +2,24 @@
   <div>
     <div v-if="playerFound">
       <h1 class="page-title">{{ player.username }}</h1>
-      <PlayerGamesList
-        :games="games"
-        :loading="gamesLoading"
-        :placeholderRows="20"
-        class="m-auto"
-        style="max-width: 710px"
-      ></PlayerGamesList>
+
+      <b-tabs style="max-width: 700px; margin: auto">
+        <b-tab title="Games" active>
+          <PlayerGamesList
+            :games="games"
+            :loading="gamesLoading"
+            :placeholderRows="20"
+            class="m-auto"
+            style="max-width: 710px"
+          ></PlayerGamesList>
+        </b-tab>
+        <b-tab title="Gods">
+          <GodStats :gods="godStats"></GodStats>
+        </b-tab>
+        <b-tab title="Abilities">
+          <AbilityStats :abilities="abilityStats"></AbilityStats>
+        </b-tab>
+      </b-tabs>
     </div>
     <div v-else>
       <h2>v-t="'profile.not_found'"</h2>
@@ -18,16 +29,22 @@
 
 <script>
 import PlayerGamesList from "./PlayerGamesList.vue";
+import GodStats from "../stats/GodStats.vue";
+import AbilityStats from "../stats/AbilityStats.vue";
 
 export default {
   components: {
     PlayerGamesList,
+    GodStats,
+    AbilityStats,
   },
 
   data: () => ({
     error: "",
     games: [],
     player: {},
+    abilityStats: [],
+    godStats: [],
     gamesLoading: true,
     playerFound: true,
   }),
@@ -51,6 +68,20 @@ export default {
       .then((player) => {
         if (player.steam_id) this.playerFound = true;
         this.player = player;
+      });
+
+    fetch(`/api/players/${this.steamID}/ability_stats`)
+      .then((res) => res.json())
+      .then((abilityStats) => {
+        this.loading = false;
+        this.abilityStats = abilityStats;
+      });
+
+    fetch(`/api/players/${this.steamID}/god_stats`)
+      .then((res) => res.json())
+      .then((godStats) => {
+        this.loading = false;
+        this.godStats = godStats;
       });
   },
 };
