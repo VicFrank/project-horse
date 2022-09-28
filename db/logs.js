0f72259b58ac6a5a68db51f40fd3496d79c37063
VicFrank <matthewshattil@gmail.com>
2022-09-28 10:55:46 -0500

    added player gods

--- db/logs.js
+++ db/logs.js
@@ -3,11 +3,13 @@ const { query } = require("./index");
 module.exports = {
   async addTransactionLog(steamID, type, transaction) {
     try {
-      const sql_query = `
-      INSERT INTO player_logs (steam_id, log_event, log_data)
-      VALUES ($1, $2, $3)
-      `;
-      await query(sql_query, [steamID, type, transaction]);
+      await query(
+        `
+        INSERT INTO player_logs (steam_id, log_event, log_data)
+        VALUES ($1, $2, $3)
+      `,
+        [steamID, type, transaction]
+      );
     } catch (error) {
       throw error;
     }
@@ -15,12 +17,14 @@ module.exports = {
 
   async getLogsOfType(type) {
     try {
-      const sql_query = `
-      SELECT * FROM player_logs
-      WHERE log_event = $1
-      ORDER BY log_time DESC
-      `;
-      const { rows } = await query(sql_query, [type]);
+      const { rows } = await query(
+        `
+        SELECT * FROM player_logs
+        WHERE log_event = $1
+        ORDER BY log_time DESC
+      `,
+        [type]
+      );
       return rows;
     } catch (error) {
       throw error;
@@ -29,16 +33,15 @@ module.exports = {
 
   async getPaypalPayments() {
     try {
-      const sql_query = `
-      SELECT steam_id, log_time,
-        log_data->'capture'->'result'->'purchase_units'->0->'payments'->'captures'->0->'seller_receivable_breakdown'->'gross_amount'->>'value' AS gross_amount,
-        log_data->'capture'->'result'->'purchase_units'->0->'payments'->'captures'->0->'seller_receivable_breakdown'->'paypal_fee'->>'value' AS paypal_fee,
-        log_data->'capture'->'result'->'purchase_units'->0->'payments'->'captures'->0->'seller_receivable_breakdown'->'net_amount'->>'value' AS net_amount
-      FROM player_logs
-      WHERE log_event = 'paypal'
-      ORDER BY log_time DESC
-      `;
-      const { rows } = await query(sql_query);
+      const { rows } = await query(`
+        SELECT steam_id, log_time,
+          log_data->'capture'->'result'->'purchase_units'->0->'payments'->'captures'->0->'seller_receivable_breakdown'->'gross_amount'->>'value' AS gross_amount,
+          log_data->'capture'->'result'->'purchase_units'->0->'payments'->'captures'->0->'seller_receivable_breakdown'->'paypal_fee'->>'value' AS paypal_fee,
+          log_data->'capture'->'result'->'purchase_units'->0->'payments'->'captures'->0->'seller_receivable_breakdown'->'net_amount'->>'value' AS net_amount
+        FROM player_logs
+        WHERE log_event = 'paypal'
+        ORDER BY log_time DESC
+      `);
       return rows;
     } catch (error) {
       throw error;
@@ -47,12 +50,11 @@ module.exports = {
 
   async getTotalPaypalPayments() {
     try {
-      const sql_query = `
-      SELECT SUM((log_data->'capture'->'result'->'purchase_units'->0->'payments'->'captures'->0->'seller_receivable_breakdown'->'net_amount'->>'value') :: DECIMAL) AS net_amount
-      FROM player_logs
-      WHERE log_event = 'paypal';
-      `;
-      const { rows } = await query(sql_query);
+      const { rows } = await query(`
+        SELECT SUM((log_data->'capture'->'result'->'purchase_units'->0->'payments'->'captures'->0->'seller_receivable_breakdown'->'net_amount'->>'value') :: DECIMAL) AS net_amount
+        FROM player_logs
+        WHERE log_event = 'paypal';
+      `);
       return rows[0].net_amount;
     } catch (error) {
       throw error;
@@ -61,14 +63,13 @@ module.exports = {
 
   async getStripePayments() {
     try {
-      const sql_query = `
-      SELECT steam_id, log_time,
-        log_data->'intent'->>'amount' AS amount
-      FROM player_logs
-      WHERE log_event = 'stripe'
-      ORDER BY log_time DESC;
-      `;
-      const { rows } = await query(sql_query);
+      const { rows } = await query(`
+        SELECT steam_id, log_time,
+          log_data->'intent'->>'amount' AS amount
+        FROM player_logs
+        WHERE log_event = 'stripe'
+        ORDER BY log_time DESC;
+      `);
       return rows;
     } catch (error) {
       throw error;
@@ -77,11 +78,10 @@ module.exports = {
 
   async getTotalStripePayments() {
     try {
-      const sql_query = `
-      SELECT SUM((log_data->'intent'->>'amount') :: DECIMAL * 0.01) AS amount
-      FROM player_logs
-      WHERE log_event = 'stripe';`;
-      const { rows } = await query(sql_query);
+      const { rows } = await query(`
+        SELECT SUM((log_data->'intent'->>'amount') :: DECIMAL * 0.01) AS amount
+        FROM player_logs
+        WHERE log_event = 'stripe';`);
       return rows[0].amount;
     } catch (error) {
       throw error;
@@ -90,12 +90,14 @@ module.exports = {
 
   async getLogsForPlayer(steamID) {
     try {
-      const sql_query = `
-      SELECT * FROM player_logs
-      WHERE steam_id = $1
-      ORDER BY log_time DESC
-      `;
-      const { rows } = await query(sql_query, [steamID]);
+      const { rows } = await query(
+        `
+        SELECT * FROM player_logs
+        WHERE steam_id = $1
+        ORDER BY log_time DESC
+      `,
+        [steamID]
+      );
       return rows;
     } catch (error) {
       throw error;
@@ -104,12 +106,14 @@ module.exports = {
 
   async getLogsOfTypeForPlayer(steamID, type) {
     try {
-      const sql_query = `
-      SELECT * FROM player_logs
-      WHERE steam_id = $1 AND log_event = $2
-      ORDER BY log_time DESC
-      `;
-      const { rows } = await query(sql_query, [steamID, type]);
+      const { rows } = await query(
+        `
+        SELECT * FROM player_logs
+        WHERE steam_id = $1 AND log_event = $2
+        ORDER BY log_time DESC
+      `,
+        [steamID, type]
+      );
       return rows;
     } catch (error) {
       throw error;
@@ -118,12 +122,14 @@ module.exports = {
 
   async getLastLogEvent(steamID, event) {
     try {
-      const sql_query = `
-      SELECT * FROM player_logs
-      WHERE steam_id = $1 AND log_event = $2
-      ORDER BY log_time DESC
-      `;
-      const { rows } = await query(sql_query, [steamID, event]);
+      const { rows } = await query(
+        `
+        SELECT * FROM player_logs
+        WHERE steam_id = $1 AND log_event = $2
+        ORDER BY log_time DESC
+      `,
+        [steamID, event]
+      );
       return rows[0];
     } catch (error) {
       throw error;
