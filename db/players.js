58cabc70894b58d393d06dd3271f6d491d3c3d46
VicFrank <matthewshattil@gmail.com>
2022-09-28 13:43:17 -0500

    turned quests back on

--- db/players.js
+++ db/players.js
@@ -2280,7 +2280,6 @@ module.exports = {
 
   async getWelcomeQuests(steamID) {
     try {
-      return [];
       const { rows } = await query(
         `SELECT player_welcome_quests.welcome_quest_id, claim_date,
           welcome_quests.day, welcome_quests.coin_reward, claim_date IS NOT NULL as claimed,
@@ -2398,7 +2397,6 @@ module.exports = {
   },
 
   async getLoginQuests(steamID) {
-    return [];
     try {
       const { rows } = await query(
         `
