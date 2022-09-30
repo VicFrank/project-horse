c9fb7602c70ad63a951fdc609e59f6a0298afe13
VicFrank <matthewshattil@gmail.com>
2022-09-29 19:44:19 -0500

    uh oh

--- db/players.js
+++ db/players.js
@@ -1042,34 +1042,34 @@ module.exports = {
           (log) => log.log_data.cosmeticName === `card_${god.god_name}`
         ).length;
         let playerGod = playerGods.find((pg) => pg.god_name === god.god_name);
-        if (!playerGod) {
-          try {
-            const numTops = await this.getNumTopsWithGod(steamID, god.god_name);
-            const progress =
-              numOpened > 1 ? numTops + (numOpened - 1) * 10 : numTops;
-            playerGod = await this.addPlayerGod(
-              steamID,
-              god.god_name,
-              progress
-            );
-          } catch (error) {
-            console.log(
-              `Error adding player god ${god.god_name} for ${steamID}`
-            );
-            console.log(error);
-            playerGod = {
-              banned: false,
-              progress: 0,
-              amount_required: 100,
-            };
-          }
-        }
+        // if (!playerGod) {
+        //   try {
+        //     const numTops = await this.getNumTopsWithGod(steamID, god.god_name);
+        //     const progress =
+        //       numOpened > 1 ? numTops + (numOpened - 1) * 10 : numTops;
+        //     playerGod = await this.addPlayerGod(
+        //       steamID,
+        //       god.god_name,
+        //       progress
+        //     );
+        //   } catch (error) {
+        //     console.log(
+        //       `Error adding player god ${god.god_name} for ${steamID}`
+        //     );
+        //     console.log(error);
+        //     playerGod = {
+        //       banned: false,
+        //       progress: 0,
+        //       amount_required: 100,
+        //     };
+        //   }
+        // }
         const isBanned = playerGod.banned;
-        const progress = Math.min(
-          playerGod.progress,
-          playerGod.amount_required
-        );
-        const amount_required = playerGod.amount_required;
+        // const progress = Math.min(
+        //   playerGod.progress,
+        //   playerGod.amount_required
+        // );
+        // const amount_required = playerGod.amount_required;
 
         god.owned =
           hasGod || god.free || (player.has_plus && god.plus_exclusive);
