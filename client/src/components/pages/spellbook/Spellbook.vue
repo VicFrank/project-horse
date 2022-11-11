76f89f9358fc808b6e88f19f4e3476185b07bdf0
Jordan <jchapman134569@gmail.com>
2022-11-11 17:41:02 -0500

    Style fully stolen from custom game

--- client/src/components/pages/spellbook/Spellbook.vue
+++ client/src/components/pages/spellbook/Spellbook.vue
@@ -91,31 +91,31 @@ export default {
       </div>
     </div>
     <div class="card-container">
-      <div v-for="ability in filteredAbilities" :key="ability.id" class="spell-card">
-        <h4 class="pt-2 pl-2" style="background: #1b182f">
+      <div v-for="ability in filteredAbilities" :key="ability.id" class="ability-tooltip">
+        <div class="ability-name">
           {{ ability.name }}
-        </h4>
-        <div style="display: flex; flex-direction: row" class="">
-          <div id="types" class="m-2">
+        </div>
+        <div style="display: flex" class="mt-2">
+          <div class="categories">
             <div v-for="catStr in ability.categories" :key="catStr">
               {{ catStr }}
             </div>
           </div>
-          <img style="margin-left: auto; margin-right: 8px"
-            :src="`../images/ability_icons/${ability.icon}.png`" />
+          <img style="margin-left: auto; margin-right: 8px" :src="`../images/ability_icons/${ability.icon}.png`" />
         </div>
-        <div id="desc" class="p-2">
+        <div class="separator"></div>
+        <div class="ability-description">
           {{ ability.description }}
         </div>
         <div id="values" class="p-2">
           <div v-for="val in ability.values" :key="val">
-            <span class="uppercase"> {{ val.split(":")[0] }}: </span>
-            <span>{{ val.split(":")[1] }}</span>
+            <span class="uppercase ability-value-desc"> {{ val.split(":")[0] }}: </span>
+            <span class="ability-value">{{ val.split(":")[1] }}</span>
           </div>
-          <div style="display: flex; flex-wrap: wrap; align-items: center">
-            <div v-if="ability.cooldowns.length > 0" class="py-2">
-              <img src="./clock.svg" width="16px" height="16px" />
-              <span>
+          <div style="display: flex; flex-wrap: wrap; align-items: center; margin-top: 8px">
+            <div v-if="ability.cooldowns.length > 0" class="icon-and-values">
+              <div id="cooldown-icon"></div>
+              <span class="ml-2">
                 {{
                     ability.cooldowns
                       .slice(0, Math.min(ability.cooldowns.length, 3))
@@ -125,8 +125,8 @@ export default {
             </div>
             <div v-if="ability.manaCost.length" class="pl-4">
               <div style="display: flex">
-                <div style="height: 16px; width: 16px; background: blue"></div>
-                <span class="pl-2">
+                <div id="mana-cost-icon"></div>
+                <span class="ml-2">
                   {{
                       ability.manaCost
                         .slice(0, Math.min(ability.manaCost.length, 3))
@@ -138,17 +138,17 @@ export default {
           </div>
         </div>
         <div id="super" class="p-2 mt-auto mb-0 ">
-          <span class="">Super: </span>
+          <span style="color: rgb(99, 117, 255)">Super: </span>
           <span class="">
             {{ ability.superUpgrade.split("Super: ")[1] }}
           </span>
         </div>
         <div id="gaben" class="p-2 mb-0">
-          <span class="">Gaben: </span>
+          <span style="color: rgb(254, 116, 0)">Gaben: </span>
           <span>{{ ability.gabenUpgrade.split("Gaben: ")[1] }}</span>
         </div>
         <div v-if="ability.differences" id="differences" class="p-2 mb-0">
-          <span>Differences from Dota: </span>
+          <span style="color: #8e8676;">Differences from Dota: </span>
           {{ ability.differences }}
         </div>
       </div>
@@ -161,26 +161,6 @@ export default {
 <style>
 .spell-search {
   display: block;
-}
-
-.spell-card {
-  display: flex;
-  flex-direction: column;
-  max-width: 20rem;
-  margin: 0.5rem;
-  background: var(--primary-color-dark)
-}
-
-#super {
-  background: rgb(32 26 79);
-}
-
-#gaben {
-  background: rgb(45 34 63);
-}
-
-#differences {
-  background: var(--primary-color);
 }
 
 .filter-selected {
@@ -204,5 +184,95 @@ export default {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+}
+
+.ability-tooltip {
+  width: 350px;
+  background-color: #1b1331;
+  margin: 8px;
+  border: 2px solid #554f9d;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.ability-name {
+  background-color: #231b40;
+  height: 40px;
+  padding: 0 8px 0 20px;
+  width: 100%;
+  font-size: 20px;
+  color: #e1e1e1;
+  font-family: 'Reaver', 'Goudy Trajan Medium', 'FZKai-Z03', 'TH Sarabun New', 'YDYGO 540';
+  font-weight: 600;
+  letter-spacing: 0px;
+  text-transform: uppercase;
+  text-shadow: 0px 1px 2px black;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.categories {
+  color: #6548a0;
+  margin-left: 8px;
+}
+
+.separator {
+  height: 2px;
+  background-color: #383167;
+  margin: 12px;
+}
+
+.ability-description {
+  color: #9a88bd;
+  margin-left: 8px;
+}
+
+.ability-value-desc {
+  color: #6548a0;
+}
+
+.ability-value {
+  color: #e1e1e1;
+}
+
+#cooldown-icon {
+  background-image: url("https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/cooldown.png");
+  width: 16px;
+  height: 16px;
+  border-radius: 3px;
+  background-size: cover;
+}
+
+#mana-cost-icon {
+  width: 16px;
+  height: 16px;
+  border-radius: 3px;
+  background: linear-gradient(#00A4DB, #007196);
+}
+
+.icon-and-values {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+#super {
+  color: #9a88bd;
+  background: linear-gradient(rgba(104, 104, 193, 0.1), rgba(31, 31, 151, 0.16));
+  box-shadow: inset 0px 0px 15px -5px rgba(98, 116, 255, 0.5);
+}
+
+#gaben {
+  color: #9a88bd;
+  box-shadow: inset 0px 0px 15px -5px rgba(156, 104, 74, 0.5);
+  background: linear-gradient(rgba(59, 41, 34, 0.8), rgba(0, 0, 0, 0));
+}
+
+#differences {
+  background: linear-gradient(rgb(43, 36, 61), rgba(38, 30, 58, 1));
+  color: #67696a;
 }
 </style>
