426e7deed6ee1fbdf0e0dfebb252637e6ccb1a80
Jordan <jchapman134569@gmail.com>
2022-11-11 21:25:47 -0500

    Reduce overall card size, space out upgrade levels, move icon
    Moved icon to top left next to name of spell
    Now have 5 cards per row on large resolution

--- client/src/components/pages/spellbook/Spellbook.vue
+++ client/src/components/pages/spellbook/Spellbook.vue
@@ -92,8 +92,11 @@ export default {
     </div>
     <div class="card-container">
       <div v-for="ability in filteredAbilities" :key="ability.id" class="ability-tooltip">
-        <div class="ability-name">
-          {{ ability.name }}
+        <div class="ability-header">
+          <img class="ability-icon" :src="`../images/ability_icons/${ability.icon}.png`" />
+          <div class="ability-name">
+            {{ ability.name }}
+          </div>
         </div>
         <div style="display: flex" class="mt-2">
           <div class="categories">
@@ -101,20 +104,19 @@ export default {
               {{ catStr }}
             </div>
           </div>
-          <img style="margin-left: auto; margin-right: 8px" :src="`../images/ability_icons/${ability.icon}.png`" />
         </div>
         <div class="separator"></div>
         <div class="ability-description">
           {{ ability.description }}
         </div>
-        <div id="values" class="p-2">
+        <div class="values p-2">
           <div v-for="val in ability.values" :key="val">
             <span class="uppercase ability-value-desc"> {{ val.split(":")[0] }}: </span>
             <span class="ability-value">{{ val.split(":")[1] }}</span>
           </div>
           <div style="display: flex; flex-wrap: wrap; align-items: center; margin-top: 8px">
-            <div v-if="ability.cooldowns.length > 0" class="icon-and-values">
-              <div id="cooldown-icon"></div>
+            <div v-if="ability.cooldowns.length > 0" class="icon-and-values pr-4">
+              <div class="cooldown-icon"></div>
               <span class="ml-2">
                 {{
                     ability.cooldowns
@@ -123,9 +125,9 @@ export default {
                 }}
               </span>
             </div>
-            <div v-if="ability.manaCost.length" class="pl-4">
+            <div v-if="ability.manaCost.length" class="icon-and-values">
               <div style="display: flex">
-                <div id="mana-cost-icon"></div>
+                <div class="mana-cost-icon"></div>
                 <span class="ml-2">
                   {{
                       ability.manaCost
@@ -137,19 +139,21 @@ export default {
             </div>
           </div>
         </div>
-        <div id="super" class="p-2 mt-auto mb-0 ">
-          <span style="color: rgb(99, 117, 255)">Super: </span>
-          <span class="">
-            {{ ability.superUpgrade.split("Super: ")[1] }}
-          </span>
-        </div>
-        <div id="gaben" class="p-2 mb-0">
-          <span style="color: rgb(254, 116, 0)">Gaben: </span>
-          <span>{{ ability.gabenUpgrade.split("Gaben: ")[1] }}</span>
-        </div>
-        <div v-if="ability.differences" id="differences" class="p-2 mb-0">
-          <span style="color: #8e8676;">Differences from Dota: </span>
-          {{ ability.differences }}
+        <div class="sink">
+          <div class="super">
+            <span style="color: rgb(99, 117, 255)">Super: </span>
+            <span class="">
+              {{ ability.superUpgrade.split("Super: ")[1] }}
+            </span>
+          </div>
+          <div class="gaben">
+            <span style="color: rgb(254, 116, 0)">Gaben: </span>
+            <span>{{ ability.gabenUpgrade.split("Gaben: ")[1] }}</span>
+          </div>
+          <div v-if="ability.differences" class="differences">
+            <span style="color: #8e8676;">Differences from Dota: </span>
+            {{ ability.differences }}
+          </div>
         </div>
       </div>
     </div>
@@ -187,31 +191,39 @@ export default {
 }
 
 .ability-tooltip {
-  width: 350px;
+  width: 210px;
   background-color: #1b1331;
-  margin: 8px;
+  margin: 4px;
   border: 2px solid #554f9d;
 
+  font-size: 14px;
   display: flex;
   flex-direction: column;
 }
 
-.ability-name {
+.ability-icon {
+  height: 50px;
+  width: 50px;
+  margin: 4px;
+  box-shadow: 0px 0px 8px #000;
+}
+
+.ability-header {
   background-color: #231b40;
-  height: 40px;
-  padding: 0 8px 0 20px;
   width: 100%;
+
+  display: flex;
+  align-items: center;
+}
+
+.ability-name {
+  margin-left: 8px;
   font-size: 20px;
   color: #e1e1e1;
   font-family: 'Reaver', 'Goudy Trajan Medium', 'FZKai-Z03', 'TH Sarabun New', 'YDYGO 540';
   font-weight: 600;
-  letter-spacing: 0px;
   text-transform: uppercase;
   text-shadow: 0px 1px 2px black;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
 }
 
 .categories {
@@ -238,7 +250,7 @@ export default {
   color: #e1e1e1;
 }
 
-#cooldown-icon {
+.cooldown-icon {
   background-image: url("https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/cooldown.png");
   width: 16px;
   height: 16px;
@@ -246,7 +258,7 @@ export default {
   background-size: cover;
 }
 
-#mana-cost-icon {
+.mana-cost-icon {
   width: 16px;
   height: 16px;
   border-radius: 3px;
@@ -257,22 +269,33 @@ export default {
   display: flex;
   flex-direction: row;
   align-items: center;
+  padding-top: 4px;
 }
 
-#super {
+.sink {
+  margin-top: auto;
+}
+
+.super {
   color: #9a88bd;
   background: linear-gradient(rgba(104, 104, 193, 0.1), rgba(31, 31, 151, 0.16));
   box-shadow: inset 0px 0px 15px -5px rgba(98, 116, 255, 0.5);
+  padding: 8px;
+  margin-top: 12px;
 }
 
-#gaben {
+.gaben {
   color: #9a88bd;
   box-shadow: inset 0px 0px 15px -5px rgba(156, 104, 74, 0.5);
   background: linear-gradient(rgba(59, 41, 34, 0.8), rgba(0, 0, 0, 0));
+  padding: 8px;
+  margin-top: 12px;
 }
 
-#differences {
+.differences {
   background: linear-gradient(rgb(43, 36, 61), rgba(38, 30, 58, 1));
   color: #67696a;
+  padding: 8px;
+  margin-top: 12px;
 }
 </style>
