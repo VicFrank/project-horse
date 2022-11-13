b2069a6ac48fa0813b61abbe230adc2be5653edf
VicFrank <matthewshattil@gmail.com>
2022-11-12 21:41:44 -0600

    added santa greevil

--- testing/initialize-gods.js
+++ testing/initialize-gods.js
@@ -1,8 +1,8 @@
 const { query } = require("../db/index");
 
-// INSERT INTO gods (god_name, free) VALUES ('selemene', false);
-// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('card_selemene', 'Card Frame', '', -1, -1, 'Immortal');
-// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('gold_card_selemene', 'Card Frame', '', -1, -1, 'Immortal');
+// INSERT INTO gods (god_name, free) VALUES ('santaGreevil', false);
+// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('card_santaGreevil', 'Card Frame', '', -1, -1, 'Immortal');
+// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('gold_card_santaGreevil', 'Card Frame', '', -1, -1, 'Immortal');
 async function InitializeGods() {
   const gods = [
     "dazzle",
