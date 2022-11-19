db7e54630b42b914ab5f393c2c2053c9d73121a8
Jordan <jchapman134569@gmail.com>
2022-11-19 00:57:05 -0500

    It's mostly working, probably a good time to commit

--- client/src/components/pages/learn/LearnGods.vue
+++ client/src/components/pages/learn/LearnGods.vue
@@ -25,42 +25,58 @@ export default {
     <h1 class="page-title">{{ $t("gods.page_title") }}</h1>
     <div class="gods-container">
       <div v-for="god of gods" :key="god.id">
-        <div class="GodCardContainer" :class="{ plus: god.unlock === 'plus' }">
-          <img class="avatar" :src="`/images/gods/${god.id}.png`">
-          <img class="card-frame" src="./god_card_frame_blank.png">
-          <div class="card-banner">
-            <div class="card-banner_title">{{ god.name }}</div>
-            <img class="card-banner_plus-icon" :class="{ hidden: god.unlock !== 'plus' }"
-              src="./user_bar_plus_icon.png">
-          </div>
-          <div class="abilities-container">
-            <div v-for="power of god.powers" :key="power.id" class="ability-container" :id="power.id">
-              <div class="ability-background">
-                <b-img v-bind:src="`/images/gods/powers/power_${power.id}.png`" class="ability-icon"></b-img>
+        <div class="PreSelectedGod">
+          <div class="GodCardContainer" :class="{Plus: god.unlock === 'plus'}">
+            <!-- TOOD scrape this data :O -->
+            <img class="GodComplexity" src="./god_complexity_easy.png">
+            <img class="GodCard_Avatar" :src="`/images/gods/${god.id}.png`">
+            <img class="GodCard_Background" src="./god_card_frame_blank.png">
+            <div class="GodCard_BannerContainer">
+              <div class="GodCard_BannerContent">
+                <span>{{ god.name }}</span>
+                <img v-if="god.unlock === 'plus'" src="./user_bar_plus_icon.png">
               </div>
-              <img v-if="power.type !== 'passive'" src="./active_ability_frame.png" class="ability-frame">
-              <b-tooltip :target="power.id" custom-class="PowerTooltipContainer" placement="bottom">
-                <div class="PowerTooltip GenericTooltip">
-                  <div class="GenericTooltip__Header">
-                    {{ power.name }}
-                  </div>
-                  <div class="GenericTooltip__Body">
-                    <div class="Type">
-                      {{ power.type_string }}
+            </div>
+            <img class="GodCard_Universe" v-if="god.universe !== 'none'"
+              :src="`/images/logos/${god.universe}_logo.png`">
+            <div class="GodCard_Info" :id="`${god.id}_info`" />
+            <b-tooltip :target="`${god.id}_info`" custom-class="" placement="left">
+              <div class="" style="padding: 10px;">
+                {{ god.unlock_text }}
+              </div>
+            </b-tooltip>
+            <div class="GodCard_Abilities">
+              <div v-for="power of god.powers" :key="power.id" :id="power.id" class="GodCard_PowerButton"
+                :class="{ SmallPowers: god.powers.length > 2, PassiveAbility: power.type === 'passive' }">
+                <div class="GodCard_AbilityBackground">
+                  <img class="GodCard_Ability" :src="`/images/gods/powers/power_${power.id}.png`">
+                  <img class="GodCard_AbilityFrame" v-if="power.type !== 'passive'" src="./active_ability_frame.png">
+                </div>
+                <b-tooltip :target="power.id" custom-class="PowerTooltipContainer" placement="bottom">
+                  <div class="PowerTooltip GenericTooltip">
+                    <div class="GenericTooltip__Header">
+                      {{ power.name }}
                     </div>
-                    <div class="Description">
-                      {{ power.desc }}
-                    </div>
-                    <div class="Values">
-                      <div v-for="value in power.values" :key="value" class="ValueContainer">
-                        <span class="ValueLabel">{{value.split(":")[0]}}:</span>
-                        <span class="Value">{{value.split(":")[1]}}</span>
+                    <div class="GenericTooltip__Body">
+                      <div class="Type">
+                        {{ power.type_string }}
+                      </div>
+                      <div class="Description">
+                        {{ power.desc }}
+                      </div>
+                      <div v-if="power.values.length > 0 && power.values[0].length > 0" class="Values">
+                        <div v-for="value in power.values" :key="value" class="ValueContainer">
+                          <span class="ValueLabel">{{ value.split(":")[0] }}:</span>
+                          <span class="Value">{{ value.split(":")[1] }}</span>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </b-tooltip>
+                </b-tooltip>
+              </div>
             </div>
+            <!-- TODO scrape health -->
+            <div class="GodCard_Health">200{{ god.health }}</div>
           </div>
         </div>
       </div>
@@ -74,198 +90,118 @@ export default {
   flex-wrap: wrap;
 }
 
+.PreSelectedGod {
+  align: center center;
+  flow-children: down;
+}
 
-/*
-.GodCardContainer {
-    width: 275px;
-    height: 300px;
-    padding: 5px 10px 2px 7px;
-    opacity: 0;
-    margin-top: 45px;
-    margin-bottom: 45px;
-    transform: rotateY(-90deg);
-    brightness: 100;
-    overflow: noclip;
-    */
-.GodCardContainer {
-  width: 275px;
-  height: 300px;
-  padding: 5px 10px 2px 7px;
-
+.PreSelectedGod .GodCardContainer {
+  margin-top: 0;
   display: flex;
   flex-direction: column;
-  align-items: center;
   position: relative;
-}
-
-.plus {
-  filter: drop-shadow(0px 3px 3px rgba(0, 204, 255, 0.5));
-  border-radius: 30px;
-}
-
-.card-frame {
-  position: absolute;
   width: 258px;
-  height: 292px;
+
+  align: center center;
+  saturation: 0;
+  brightness: 0.1;
 }
 
-/*
-    BannerContainer {
-        align: left top;
-        margin-top: 24px;
-        width: $cardWidth;
-    }
-    */
-.card-banner {
-  position: absolute;
-  margin-top: 24px;
-  width: 100%;
-  text-align: center;
+.GodCardContainer.Plus {
+  filter: drop-shadow(0 3px 3px rgba(0, 204, 255, .5));
 }
 
-/*
-    &_BannerContent {
-        align: center center;
-        flow-children: right;
-
-        Image {
-            height: $plusIconSize;
-            width: $plusIconSize;
-            margin-top: -10px;
-            border-radius: 50%;
-            visibility: collapse;
-            box-shadow: rgba(0, 0, 0, 0.25) 0px 0px 10px 0px;
-        }
-
-        Label {
-            text-align: center;
-            font-size: 14px;
-            font-family: titleFont;
-            color: gradient(linear, 0% 0%, 0% 100%, from(#fad2f8), to(#d677ce));
-            text-shadow: black 0px 1px 2px 2;
-        }
-    }
-*/
-
-.card-banner_title {
-  text-align: center;
-  font-size: 14px;
-  color: #fad2f8;
-  text-shadow: 0px 1px 2px black;
+.PreSelectedGod .GodCard_Avatar {
+  background-color: black;
 }
 
-.card-banner_plus-icon {
-  height: 42px;
-  width: 42px;
+.PreSelectedGod .ButtonsContainer {
+  flow-children: right;
+}
+
+.PreSelectedGod .ButtonsContainer .Lock {
   margin-top: -30px;
-  margin-left: 130px;
-  border-radius: 50%;
+  height: 40px;
+  width: 30px;
+  align: center top;
 }
 
-/*
-&_Avatar {
+.PreSelectedGod .ButtonsContainer .FancyButton {
+  margin-right: 10px;
+}
+
+.GodComplexity {
+  height: 55px;
+  width: 55px;
+  margin: auto;
+  align: center top;
+  saturation: 0.3;
+}
+
+.GodCard_Avatar {
   width: 175px;
   height: 175px;
+  margin-left: auto;
+  margin-right: auto;
   align: middle top;
   margin-top: 15px;
 }
-*/
-.avatar {
-  margin-top: 15px;
-  width: 175px;
-  height: 175px;
-}
 
-/*
-&_Abilities {
-  width: fit-children;
-  height: fit-children;
-  align: middle bottom;
-  margin: 0 4px 47px 0;
-  flow-children: right;
-  overflow: noclip;
-}
-*/
-.abilities-container {
-  margin-top: -10px;
-  display: flex;
-  z-index: 1;
-}
-
-.ability-container {
-  position: relative;
-  padding-right: 5px;
-  padding-left: 5px;
-}
-
-/*
-    &_AbilityBackground {
-        width: 60px;
-        height: 65px;
-        background-image: url("file://{images}/custom_game/gods/god_card_ability_box.png");
-        background-size: 120% 110%;
-        background-position: center;
-        background-repeat: no-repeat;
-    }
-*/
-
-.ability-background {
-  background-image: url("./god_card_ability_box.png");
-  background-size: 120% 110%;
-  background-position: center;
-  background-repeat: no-repeat;
-  width: 60px;
-  height: 65px;
-}
-
-/*
-    &_AbilityFrame {
-        width: 55px;
-        height: 55px;
-        align: middle middle;
-        margin-bottom: 5px;
-        opacity: 1;
-        transition: transform 0.1s ease-in-out 0s, brightness 0.1s ease-in-out 0s;
-    }
-*/
-.ability-frame {
-  width: 55px;
-  height: 55px;
+.GodCard_Background {
+  width: 258px;
+  height: 292px;
+  top: 55px;
   position: absolute;
-  top: 2px;
-  left: 7px;
 }
 
-/*
-    &_Ability {
-        width: 55px;
-        height: 55px;
-        align: middle middle;
-        margin-bottom: 5px;
-        transition: transform 0.1s ease-in-out 0s, brightness 0.1s ease-in-out 0s, saturation 0.1s ease-in-out 0s;
-    }
-
-    &_Health {
-        align: middle bottom;
-        width: 50px;
-        margin: 0 0 11px 15px;
-        text-align: center;
-        font-size: 24px;
-        font-family: monospaceNumbersFont;
-        color: gradient(linear, 0% 0%, 0% 100%, from(#eece83), to(#d9b86b));
-        text-shadow: black 0px 1px 2px 2;
-    }
-*/
-
-.ability-icon {
-  height: 55px;
-  width: 55px;
-  margin-top: 2px;
-  margin-left: 2px;
+.GodCard_BannerContainer {
+  position: absolute;
+  align: left top;
+  top: 79px;
+  width: 258px;
 }
 
-/*
-&_Universe {
+
+
+.GodCard_BannerContent {
+  text-align: center;
+  align: center center;
+  flow-children: right;
+}
+
+.GodCard_BannerContent img {
+  height: 42px;
+  width: 42px;
+  margin-top: -10px;
+  border-radius: 50%;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 0px 10px 0px;
+  position: absolute;
+}
+
+.GodCard_BannerContent span {
+  text-align: center;
+  font-size: 16px;
+  font-family: titleFont;
+  color: gradient(linear, 0% 0%, 0% 100%, from(#fad2f8), to(#d677ce));
+  text-shadow: black 0px 1px 2px 2;
+}
+
+.GodCard_Info {
+  height: 18px;
+  width: 18px;
+  align: left bottom;
+  margin: 0 0 32px 36px;
+  background-image: url("./info_icon.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  wash-color: #cfcbff;
+  opacity: 0.5;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+
+.GodCard_Universe {
   height: 30px;
   width: 30px;
   align: right bottom;
@@ -273,18 +209,84 @@ export default {
   margin: 0 32px 32px 0;
   saturation: 0;
   wash-color: #554f9d;
-}
-*/
-.universe-logo {
-  height: 30px;
-  width: 30px;
-  opacity: 0.5;
-  margin: 0 32px 32px 0;
+
   filter: saturate(0);
+
+  position: absolute;
+  bottom: 0;
+  right: 0;
 }
 
-.hidden {
-  display: none;
+.GodCard_Abilities {
+  width: fit-children;
+  height: fit-children;
+  align: middle bottom;
+  margin: -10px 4px 47px 0;
+  flow-children: right;
+  overflow: noclip;
+
+  display: flex;
+  justify-content: center;
+  position: relative;
+}
+
+.GodCard_PowerButton {
+  overflow: noclip;
+  margin: 0 2px;
+}
+
+.GodCard_AbilityBackground {
+  width: 60px;
+  height: 65px;
+  background-image: url("./god_card_ability_box.png");
+  background-size: 120% 110%;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.GodCard_Ability {
+  width: 55px;
+  height: 55px;
+  margin-bottom: 5px;
+  align: middle middle;
+}
+
+.SmallPowers .GodCard_Ability,
+.SmallPowers .GodCard_AbilityFrame {
+  width: 46px;
+  height: 46px;
+  margin: 0 1px 6px 0;
+}
+
+.SmallPowers .GodCard_Abilities {
+  margin-bottom: 58px;
+}
+
+.SmallPowers .GodCard_AbilityContainer {
+  width: 62px;
+  height: 62px;
+}
+
+.GodCard_AbilityFrame {
+  width: 55px;
+  height: 55px;
+  align: middle middle;
+  margin-bottom: 5px;
+  opacity: 1;
+  transition: transform 0.1s ease-in-out 0s, brightness 0.1s ease-in-out 0s;
+
+  position: absolute;
+}
+
+.PassiveAbility .GodCard_Ability {
+  width: 50px;
+  height: 50px;
+  box-shadow: rgba(0, 0, 0, 0.9254901961) 0px 0px 4px 1px;
 }
 
 .PowerTooltipContainer {
@@ -367,7 +369,27 @@ export default {
   color: #645b77;
   font-weight: thin;
 }
+
 .tooltip.b-tooltip {
   opacity: 1;
+}
+
+.tooltip-inner {
+  max-width: 400px;
+}
+
+.GodCard_Health {
+  align: middle bottom;
+  width: 100%;
+  margin-bottom: 17px;
+  margin-left: 9px;
+  text-align: center;
+  font-size: 24px;
+  font-family: monospaceNumbersFont;
+  color: #d9b86b;
+  text-shadow: black 0px 1px 2px 2;
+  position: absolute;
+  bottom: 0;
+  left: 0;
 }
 </style>
