3c8a42b4b2ba68b3d74a2c1dffd880a40f422546
VicFrank <matthewshattil@gmail.com>
2022-11-22 16:52:34 -0600

    Added bathtime Aghanim

--- testing/initialize-gods.js
+++ testing/initialize-gods.js
@@ -1,11 +1,9 @@
 const { query } = require("../db/index");
 
-// INSERT INTO gods (god_name, free) VALUES ('caveJohnson', false);
-// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('card_caveJohnson', 'Card Frame', '', -1, -1, 'Immortal');
-// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('gold_card_caveJohnson', 'Card Frame', '', -1, -1, 'Immortal');
-// INSERT INTO gods (god_name, free) VALUES ('chaosGodS3', false);
-// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('card_chaosGodS3', 'Card Frame', '', -1, -1, 'Immortal');
-// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('gold_card_chaosGodS3', 'Card Frame', '', -1, -1, 'Immortal');
+// INSERT INTO gods (god_name, free) VALUES ('bathtimeAghanim', false);
+// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('card_bathtimeAghanim', 'Card Frame', '', -1, -1, 'Immortal');
+// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('gold_card_bathtimeAghanim', 'Card Frame', '', -1, -1, 'Immortal');
+
 async function InitializeGods() {
   const gods = [
     "dazzle",
