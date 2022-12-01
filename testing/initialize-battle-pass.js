1bd03dbca0ff31331b0f103c47f04d72c0426f66
VicFrank <matthewshattil@gmail.com>
2022-12-01 16:06:16 -0600

    fix

--- testing/initialize-battle-pass.js
+++ testing/initialize-battle-pass.js
@@ -201,8 +201,8 @@ async function fixUninitializedPlayers() {
 
 (async () => {
   await fixUninitializedPlayers();
-  await claimRewardsForAllPlayers();
+  // await claimRewardsForAllPlayers();
 
-  await deleteBattlePasses();
-  await initializeBattlePass();
+  // await deleteBattlePasses();
+  // await initializeBattlePass();
 })();
