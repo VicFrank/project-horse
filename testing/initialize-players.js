f321d58748d2cfe03d17cd91cf8417db16ed02a4
VicFrank <matthewshattil@gmail.com>
2023-01-01 12:35:07 -0600

    prepared scripts for new patch

--- testing/initialize-players.js
+++ testing/initialize-players.js
@@ -275,8 +275,6 @@ async function addOldBattlepassIcons() {
 }
 
 (async () => {
-  // await updateGodChests();
-  // await giveEndOfSeasonRewards();
-  // await ladderReset();
-  await addOldBattlepassIcons();
+  await giveEndOfSeasonRewards();
+  await ladderReset();
 })();
