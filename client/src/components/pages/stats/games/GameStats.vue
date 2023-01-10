61cb8a1b7c43adddf2585cde8cef8d5a8676b670
Jordan <jchapman134569@gmail.com>
2023-01-09 20:45:11 -0500

    Add charts for game statistics

--- client/src/components/pages/stats/games/GameStats.vue
+++ client/src/components/pages/stats/games/GameStats.vue
@@ -67,12 +67,19 @@
         sub-title="Average Number of Rounds">
       </b-card>
     </div>
+    <GameLineGraph :chart-data="gamesPlayedData" :loading="loadingCharts" :title="gamesPlayedTitle" />
+    <GameLineGraph :chart-data="roundsData" :loading="loadingCharts" :title="roundsTitle" />
+    <GameLineGraph :chart-data="durationData" :loading="loadingCharts" :title="durationTitle" />
   </div>
 </template>
 
 <script>
+import GameLineGraph from './GameLineGraph.vue';
 
 export default {
+  components: {
+    GameLineGraph,
+  },
 
   data: () => ({
     loading: true,
@@ -86,27 +93,41 @@ export default {
     customDateStart: '2022-12-25',
     customDateEnd: '2022-12-25',
 
+    loadingCharts: true,
+
+    gamesPlayedData: {labels: [], datasets: []},
+    gamesPlayedTitle: "Loading...",
+    roundsData: {labels: [], datasets: []},
+    roundsTitle: "Loading...",
+    durationData: {labels: [], datasets: []},
+    durationTitle: "Loading...",
   }),
 
   created() {
+    const dailyGraph = {title: "Daily ", endpoint: "/games/daily"};
+    const weeklyGraph = {title: "Weekly ", endpoint: "/games/weekly"};
+    const monthlyGraph = {title: "Monthly ", endpoint: "/games/monthly"};
+    const perPatchGraph = {title: "Per Patch ", endpoint: "/games/perPatch"};
+
     this.dateOptions = [
-      { value: { startDate: '2022-09-01', endDate: this.daysAgo(0) }, text: "All Time" },
+      { text: "All Time", value: { startDate: '2022-09-01', endDate: this.daysAgo(0), graph: perPatchGraph } },
       { value: null, text: "------------", disabled: true },
-      { value: { startDate: this.daysAgo(7), endDate: this.daysAgo(0) }, text: "Week" },
-      { value: { startDate: this.daysAgo(30), endDate: this.daysAgo(0) }, text: "Month" },
-      { value: { startDate: this.daysAgo(365), endDate: this.daysAgo(0) }, text: "Year" },
+      { text: "Week", value: { startDate: this.daysAgo(7), endDate: this.daysAgo(0), graph: dailyGraph } },
+      { text: "Month", value: { startDate: this.daysAgo(30), endDate: this.daysAgo(0), graph: weeklyGraph } },
+      { text: "Year", value: { startDate: this.daysAgo(365), endDate: this.daysAgo(0), graph: monthlyGraph } },
       { value: null, text: "------------", disabled: true },
-      { value: { startDate: "2022-07-08", endDate: "2022-11-01" }, text: "Season 1" },
-      { value: { startDate: "2022-10-05", endDate: "2022-11-01" }, text: "Season 1.5" },
-      { value: { startDate: "2022-11-01", endDate: "2022-12-01" }, text: "Season 2" },
-      { value: { startDate: "2022-11-14", endDate: "2022-12-01" }, text: "Season 2.5" },
-      { value: { startDate: "2022-12-01", endDate: "2023-01-01" }, text: "Season 3" },
-      { value: { startDate: "2022-12-15", endDate: "2023-01-01" }, text: "Season 3.5" },
-      { value: { startDate: "2023-01-01", endDate: "2023-03-01" }, text: "Season 4" },
+      { text: "Season 1", value: { startDate: "2022-07-08", endDate: "2022-11-01", graph: perPatchGraph } },
+      { text: "Season 1.5", value: { startDate: "2022-10-05", endDate: "2022-11-01", graph: perPatchGraph } },
+      { text: "Season 2", value: { startDate: "2022-11-01", endDate: "2022-12-01", graph: perPatchGraph } },
+      { text: "Season 2.5", value: { startDate: "2022-11-14", endDate: "2022-12-01", graph: perPatchGraph } },
+      { text: "Season 3", value: { startDate: "2022-12-01", endDate: "2023-01-01", graph: perPatchGraph } },
+      { text: "Season 3.5", value: { startDate: "2022-12-15", endDate: "2023-01-01", graph: perPatchGraph } },
+      { text: "Season 4", value: { startDate: "2023-01-01", endDate: "2023-03-01", graph: perPatchGraph } },
       { value: null, text: "------------", disabled: true },
-      { value: "custom", text: "Custom Range" },
+      { value: "custom", text: "Custom Range", graph: weeklyGraph },
     ]
     this.selectedDate = this.dateOptions.find(opt => opt.text == "Month").value;
+
     this.loadGameStats();
   },
 
@@ -119,6 +140,8 @@ export default {
         endDate = this.customDateEnd;
       }
       this.loading = true;
+      this.loadingCharts = true;
+
       fetch(
         `/api/stats/games?startDate=${startDate}&endDate=${endDate}`
       )
@@ -132,6 +155,49 @@ export default {
 
           this.loading = false;
         });
+
+      fetch(`/api/stats${this.selectedDate.graph.endpoint}`)
+        .then((r) => r.json())
+        .then(historicalData => {
+          const baseTitle = this.selectedDate.graph.title;
+          const labels = historicalData.map(datum => datum.label);
+
+          this.gamesPlayedData.labels = labels;
+          this.gamesPlayedTitle = baseTitle + "Games Played";
+          this.gamesPlayedData.datasets = [
+            {
+              label: "Games Played" ,
+              borderColor: "#A9CF54",
+              backgroundColor: "#A9CF54",
+              data: historicalData.map(datum => datum.games_count)
+            },
+          ]
+
+          this.roundsTitle = baseTitle + "Average Number of Rounds";
+          this.roundsData.labels = labels;
+          this.roundsData.datasets = [
+            {
+              label: "Average Rounds" ,
+              borderColor: "#A9CF54",
+              backgroundColor: "#A9CF54",
+              data: historicalData.map(datum => datum.avg_rounds)
+            },
+          ]
+
+          this.durationTitle = baseTitle + "Average Game Duration";
+          this.durationData.labels = labels;
+          this.durationData.datasets = [
+            {
+              label: "Average Duration (minutes)" ,
+              borderColor: "#A9CF54",
+              backgroundColor: "#A9CF54",
+              data: historicalData.map(datum => datum.avg_duration/60)
+            },
+          ]
+
+          this.loadingCharts = false;
+          console.log(historicalData);
+        })
     },
     daysAgo(days) {
       const d = new Date(new Date() - days * 24 * 60 * 60 * 1000);
