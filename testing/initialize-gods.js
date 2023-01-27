33a15242a9427ec5828ad4d655e6f4e0a4024766
VicFrank <matthewshattil@gmail.com>
2023-01-27 00:44:51 -0600

    added new gods

--- testing/initialize-gods.js
+++ testing/initialize-gods.js
@@ -1,12 +1,12 @@
 const { query } = require("../db/index");
 
-// INSERT INTO gods (god_name, free, god_enabled) VALUES ('counterTerrorist', false, false);
-// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('card_counterTerrorist', 'Card Frame', '', -1, -1, 'Immortal');
-// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('gold_card_counterTerrorist', 'Card Frame', '', -1, -1, 'Immortal');
+// INSERT INTO gods (god_name, free, god_enabled) VALUES ('drKleiner', false, false);
+// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('card_drKleiner', 'Card Frame', '', -1, -1, 'Immortal');
+// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('gold_card_drKleiner', 'Card Frame', '', -1, -1, 'Immortal');
 
-// INSERT INTO gods (god_name, free, god_enabled) VALUES ('sideshop_man', false, false);
-// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('card_sideshop_man', 'Card Frame', '', -1, -1, 'Immortal');
-// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('gold_card_sideshop_man', 'Card Frame', '', -1, -1, 'Immortal');
+// INSERT INTO gods (god_name, free, god_enabled) VALUES ('medic', false, false);
+// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('card_medic', 'Card Frame', '', -1, -1, 'Immortal');
+// INSERT INTO cosmetics (cosmetic_name, cosmetic_type, equip_group, cost_coins, cost_usd, rarity) VALUES ('gold_card_medic', 'Card Frame', '', -1, -1, 'Immortal');
 
 async function InitializeGods() {
   const gods = [
