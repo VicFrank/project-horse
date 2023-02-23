ad0a73b0e05af37ceccb2fe2643e69fd833ad5a9
Jordan Chapman <jchapman@ara.com>
2023-02-22 22:44:14 -0500

    Add a failing test :)

--- db/battlepass.test.js
+++ db/battlepass.test.js
@@ -1,15 +1,33 @@
 const { getNumClaimableRewardsAtLevel } = require("./battlepass");
 
-describe("Unlocked battle pass rewards < 80", () => {
-  const testVals = Array.from({ length: 80 }).map((e, i) => ({
+describe("Unlocked battle pass rewards <= 80", () => {
+  const unlockedRewards = Array.from({ length: 80 }).map((e, i) => ({
     level: i + 1,
     expectedTotalRewards: i + 1,
   }));
-  test.each(testVals)(
+  test.each(unlockedRewards)(
     "Level $level has $expectedTotalRewards total rewards",
     ({ level, expectedTotalRewards }) => {
       expect(
-        getNumClaimableRewardsAtLevel(level, expectedTotalRewards, true)
+        getNumClaimableRewardsAtLevel(level, true)
+      ).toBe(expectedTotalRewards);
+    }
+  );
+});
+
+describe("Free battle pass rewards <= 80", () => {
+  // Every 5 levels you get a reward and a reward at level 1
+  let totalRewards = 1;
+  const freeRewards = Array.from({ length: 5 }).map((e, i) => {
+    if ((i + 1) % 5 == 0)
+      totalRewards++;
+    return { level: i + 1, expectedTotalRewards: totalRewards }
+  });
+  test.each(freeRewards)(
+    "Level $level has $expectedTotalRewards total rewards",
+    ({ level, expectedTotalRewards }) => {
+      expect(
+        getNumClaimableRewardsAtLevel(level, false)
       ).toBe(expectedTotalRewards);
     }
   );
