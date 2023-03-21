3e6fa22b522934008df758595ea0630b58078920
Jordan <jchapman134569@gmail.com>
2023-03-21 08:15:22 -0400

    Add 5.0b changelogs

--- client/src/components/pages/changelog/components/LatestChanges.vue
+++ client/src/components/pages/changelog/components/LatestChanges.vue
@@ -1,197 +1,102 @@
 <template>
   <div>
     <div v-if="$i18n.locale == 'en'">
-      <h2>Update 5.0 3/1/2023</h2>
-
-      <h3>Featured</h3>
-
-      <ul>
-        <li>7 New Gods including the likes of Icefrog and Roshan!</li>
-        <li>Galaxy God Chest 2 (which can be Recycled into the original God Chest 1)</li>
-        <li>Season 5 Battlepass including Arena Chests, Finisher Chests, Click FX and more!</li>
-        <li>3 new Arenas (2Fort, Portal, Gaben)</li>
-        <li>Now works with the Dota 2 Tooltips Extension on Twitch</li>
-      </ul>
+      <h2>Update 5.0b 3/15/2023</h2>
 
       <h3>General</h3>
 
       <ul>
-        <li>Game is now supported by the Dota 2 Tooltips Extension. Will automatically work for streams that have the
-          extension
-          installed.</li>
-        <li>Removed ready button</li>
-        <li>Tied rounds now properly shows both Finisher Effects</li>
-        <li>Hotkey Added to sell spells (Ctrl+Left Click)</li>
-        <li>God Pick Screen adjusted for more Gods and made confirming a God more obvious</li>
-        <li>If you do not pick a God in time, it will select the God you have highlighted instead of randoming</li>
-        <li>Added an effect for when you haven’t spent 3 or more gold in order to provide a visual cue to new players that
-          gold
-          resets</li>
-        <li>Added Golden Card progress tracker</li>
+        <li>Muerta added as an S tier Body</li>
+        <li>Fixed Ctrl+Click selling bugging out</li>
+        <li>Year of PLUS updated to give the newest God chest</li>
+        <li>You can now rearrange spells onto empty slots</li>
       </ul>
 
       <h3>Spells</h3>
 
       <router-link to="/learn/spells">View all Spells</router-link>
       <ul>
-        <li>Benched Berskerker's Rage</li>
-        <li>Benched Swashbuckle</li>
-        <li>Added Finger of Death</li>
-        <li>Added Press the Attack</li>
-        <li>Added Sleight of Fist</li>
-        <li>Blade Fury Gaben -8s CDR to -6s CDR</li>
-        <li>Cursed Crown stun delay reduced by 1 second</li>
-        <li>Doom Gaben is now instant cast</li>
-        <li>Ghost Ship Gaben additional ships do not cast Tidal Wave</li>
-        <li>Ice Blast duration from 3/5/7 to 5/7/9</li>
-        <li>Ice Path cast range from 1200 to 1000</li>
-        <li>Ice Path path distance from 1200 to 1000</li>
-        <li>Leech Seed Super puddle radius from 350 to 300</li>
-        <li>Leech Seed Super damage/heal per second from 100 to 50</li>
-        <li>Press the Attack AI prioritizes healing allies below 80% HP (in range)</li>
-        <li>Pulse Nova Super no longer grants Bloodstone</li>
-        <li>Pulse Nova Super now casts level 2 Diabolic Edict while active</li>
-        <li>Pulse Nova Gaben no longer grants Nihilism</li>
-        <li>Pulse Nova Gaben now grants Bloodstone</li>
-        <li>Ravage Gaben now additionally grants +600 radius</li>
-        <li>Spirit Link attack speed from 16/32/64 to 14/28/56</li>
-        <li>Spirit Link Shared Lifesteal from 10/20/40 to 7/14/28</li>
-        <li>Sprit Link Super Shared Lifesteal from +30% to +20%</li>
-        <li>Starbreaker Super no longer grants +30 swipe damage.</li>
-        <li>Starbreaker Super now additionally grants Desolator</li>
-        <li>Sticky Napalm Super no longer applies Sticky Napalm in an AOE (applies only on the target hit)</li>
-        <li>Sticky Napalm Gaben now additionally makes Sticky Napalms on attack apply in a 375 AOE</li>
-        <li>Windrun evasion from 100% to 80%</li>
+        <li>Benched Empower</li>
+        <li>Benched Coup de Gras</li>
+        <li>Enabled Wave of Terror</li>
+        <li>Enabled Sunder</li>
+        <li>Battle Trance Gaben fixed to actually work (was previously doing nothing)</li>
+        <li>Crippling Fear changed so that players in the same relative starting position can now cast it simultaneously.
+          Previously it would choose a random person</li>
+        <li>False Promise fixed to not work through to the next round sometimes</li>
+        <li>Essence Shift Super fixed to not give multiple stacks sometimes</li>
+        <li>Mortimer's Kisses AI fixed to target farthest unit from you instead of farthest from arena center</li>
+        <li>Searing Arrows Gaben Burning Army skeletons no longer proc attack modifiers aside from Searing Arrows</li>
       </ul>
 
       <h3>Gods</h3>
 
       <router-link to="/learn/gods">View all Gods</router-link>
       <ul>
-        <li>Added Primal Beast God (Battlepass Level 10) - Concept by 3rd place winner of Cardsmith contest (october)</li>
-        <li>Added Dr. Kleiner God (Battlepass Level 21) - Concept by 2nd place winner of Cardsmith contest (Pio_Romero)</li>
-        <li>Added Roshan God (Battlepass Level 63)</li>
-        <li>Added Medic God (Battlepass Level 80)</li>
-        <li>Added Brewmaster God (Rare in God Chest 2)</li>
-        <li>Added Counter-Terrorist God (Very Rare in God Chest 2)</li>
-        <li>Added Icefrog God (Extremely Rare in God Chest 2) - Concept by 1st place winner of Cardsmith contest (ubRtstcl)
-        </li>
-        <li>Boomer: Bile Explosion damage from 20% to 25%</li>
-        <li>Boomer: Bile Explosion radius from 350 to 375</li>
-        <li>Faceless Void: The March of Time chance from 35% to 30%</li>
-        <li>Phantom Assassin: PA crit damage per units moved from 2% to 2.5%</li>
-        <li>Prellex Neutral Changes:</li>
-        <ul>
-          <li>Black Drake Magic Amp Aura from 15 to 12%</li>
-          <li>Forest Hill Troll Priest Heal Amp from 10 to 8%</li>
-          <li>Harpy Stormcrafter Chain Lightning cooldown from 4 to 6</li>
-          <li>Harpy Stormcrafter Chain Lightning damage from 140 to 100</li>
-          <li>Ogre Bruiser Ogre Smash Radius from 300 to 240</li>
-          <li>Ogre Bruiser Ogre Smash Damage from 15 to 10%</li>
-          <li>Satyr Mindstealer Mana Regeneration Aura from 10 to 8</li>
-          <li>Satyr Mindstealer Mana Burn from 100 base damage to 40</li>
-          <li>Satyr Hellcaller Shockwave from 300 damage to 240</li>
-          <li>Satyr Hellcaller Health Regen from 10 to 8</li>
-          <li>Satyr Trickster Purge Cooldown from 8 to 12</li>
-          <li>Tomato Thunder Clap Damage from 300 to 240</li>
-          <li>Tomato Thunder Clap Attack speed & Movespeed Slow from 55 to 45%</li>
-        </ul>
-        <li>Rubick: Spell Steal cooldown from 20/12/4 to 12/8/4</li>
-        <li>Rubick: Points required to level Spell Steal from 0/5/15/25/35/45/55/65/75 to 0/7/14/21/28/35/42/49/56/63</li>
-        <li>Sideshop Man: No longer grants a discount on body rerolls</li>
-        <li>Terrorist Plant Bomb timer from 18/16/14/12/10 to 20/18/16/14/12</li>
-        <li>Terrorist Plant Bomb explosion damage from 90/80/70/60/50% to 80/70/60/50/40%</li>
+        <li>Brewmaster Storm Brewling Wind Walk damage from 0/20/40/60/80 to 0/60/120/180/240</li>
+        <li>Brewmaster Storm Brewling will now have a minimum of 900 cast range for all 3 of his abilities (from 600).
+          This is to prevent him from running into melee range. The range of this spell will be increased if you have a
+          spell equipped with a longer cast range. In addition, the attack range of the unit is temporarily boosted to 900
+          while using the invisibility spell</li>
+        <li>Brewmaster Storm Brewling will no longer cast dispel unless an enemy is in range</li>
+        <li>Brewmaster Storm Brewling spells cost mana now</li>
+        <li>Brewmaster Fire Brewling Immolation damage from 0/25/50/75/100 to 0/32/64/96/128</li>
+        <li>Brewmaster Earth Brewling Demolish attack damage from 0/20/40/60/80 to 0/25/50/75/100</li>
+        <li>CT equipment now shows AOE when hovering over the skill in the bottom right</li>
+        <li>CT fixes for HE grenade sometimes not working</li>
+        <li>Dr. Kleiner shop now rerolls for every 2 spells purchased</li>
+        <li>Dr Kleiner shop no longer rerolls at the end of a round</li>
+        <li>Icefrog God Tab added to show tooltip of your chosen spell</li>
+        <li>Icefrog: Spell choices at beginning of game from 5 to 4</li>
+        <li>Roshan Spell Block Level 2 additionally gives permanent 50% status resistance</li>
+        <li>Roshan Slam Level 3 primary attribute reduction from 15 to 25%</li>
+        <li>Roshan Bash Level 2 additionally improves proc chance from 15 to 20%</li>
+        <li>Shopkeeper shop size from 2/3/4 to 2/3/5</li>
+        <li>Vacation Aghanim proc chance from 5% to 7%</li>
       </ul>
     </div>
     <div v-if="$i18n.locale == 'cn'">
-      <h2>5 2023-3-1</h2>
-
-      <h3>特别推荐</h3>
-      <ul>
-        <li>已纳入 7 大新神（比如冰蛙和肉山）！</li>
-        <li>星系神宝箱 2（可回收为原始的神之宝箱 1）</li>
-        <li>第 5 赛季战斗通行证中包括竞技场宝箱、终结技宝箱、点击效果等！</li>
-        <li>3 个新竞技场（2Fort、传送门、G 胖）</li>
-      </ul>
+      <h2>5.0b 2023-3-15</h2>
 
       <h3>通用</h3>
       <ul>
-        <li>移除了就绪按钮</li>
-        <li>平局时将正确显示双方的终结技效果</li>
-        <li>已添加出售法术热键（Ctrl + 鼠标左键）</li>
-        <li>神选择页面进行了调整以容纳更多神，也让确认神的提示更加明显</li>
-        <li>如果你没有及时选择神，其会选择你已突出显示的神而不是随机选择</li>
-        <li>添加了一个效果以在没有花费 3 个或更多金币时为新玩家提供金币重置的视觉提示</li>
-        <li>添加了黄金卡片进度追踪器</li>
+        <li>已将琼英碧灵作为 S 阶身体添加</li>
+        <li>修复了“Ctrl + 点击”出售导致 Bug 的问题</li>
+        <li>如果你没有拿到所有物品则竞技场和地形宝箱将固定不提供重复物品</li>
+        <li>更新了年度 PLUS 以提供最新神之宝箱</li>
       </ul>
 
       <h3>法术</h3>
       <router-link to="/learn/spells">查看法术</router-link>
       <ul>
-        <li>已将“狂战士狂暴”加入候选</li>
-        <li>新增了“强攻”</li>
-        <li>新增了“无影拳”</li>
-        <li>剑刃风暴 G 胖版 -8 秒 CDR 调整为 -6 秒 CDR</li>
-        <li>诅咒王冠眩晕延迟缩短 1 秒</li>
-        <li>末日 G 胖版现可即时施放</li>
-        <li>幽灵船 G 胖版额外船只不会施放潮汐波</li>
-        <li>冰晶爆轰持续时间从 3/5/7 调整为 5/7/9</li>
-        <li>冰封路径施法距离从 1200 调整为 1000</li>
-        <li>冰封路径距离从 1200 调整为 1000</li>
-        <li>寄生种子超级版水潭范围从 350 调整为 300</li>
-        <li>寄生种子超级版每秒伤害/治疗从 100 调整为 50</li>
-        <li>强攻 AI 优先治疗生命值低于 80% 的友方（范围内）</li>
-        <li>脉冲新星超级版不再提供血精石</li>
-        <li>脉冲新星超级版现会在生效期间施放 2 级恶魔敕令</li>
-        <li>脉冲新星 G 胖版不再提供虚无主义</li>
-        <li>脉冲新星 G 胖版现会提供血精石</li>
-        <li>毁灭 G 胖版现还会额外提供 +600 范围</li>
-        <li>幽魂链接攻击速度从 16/32/64 调整为 14/28/56</li>
-        <li>幽魂链接共享吸血从 10/20/40 调整为 7/14/28</li>
-        <li>幽魂链接超级版共享吸血从 +30% 调整为 +20%</li>
-        <li>星破天惊超级版不再提供 +30 挥击伤害。</li>
-        <li>星破天惊超级版现还会额外提供黯灭</li>
-        <li>粘性燃油超级版不再会在 AOE 内施加粘性燃油（仅会在击中目标时施加）</li>
-        <li>粘性燃油 G 胖版现还会在 375 AOE 内施加将攻击粘性燃油</li>
-        <li>风行闪避从 100% 调整为 80%</li>
+        <li>已将“授予力量”加入候选</li>
+        <li>已将“恩赐解脱”加入候选</li>
+        <li>启用了“恐怖心潮”</li>
+        <li>启用了“魂断”</li>
+        <li>更改了伤残恐惧以使玩家现可同时施放。之前其会随机选择一人。</li>
       </ul>
 
       <h3>神</h3>
       <router-link to="/learn/gods">查看所有神</router-link>
       <ul>
-        <li>添加了獸神（10 级战斗通行证）- 卡匠大赛第三名优胜者（october）提供概念</li>
-        <li>添加了克莱纳博士神（21 级战斗通行证）- 卡匠大赛第三名优胜者（Pio_Romero）提供概念</li>
-        <li>添加了肉山神（63 级战斗通行证）</li>
-        <li>添加了医务人员神（80 级战斗通行证）</li>
-        <li>添加了酒仙神（神之宝箱 2，稀有）</li>
-        <li>添加了反恐精英神（神之宝箱 2，非常稀有）</li>
-        <li>添加了冰蛙神（神之宝箱 2，极为稀有）- 卡匠大赛第一名优胜者（ubRtstcl）提供概念</li>
-        <li>爆炸波：胆汁爆炸伤害从 20% 提高至 25%</li>
-        <li>爆炸波：胆汁爆炸范围从 350 调整为 375</li>
-        <li>虚空假面：时间向前几率从 35% 调整为 30%</li>
-        <li>幻影刺客：每单位致命一击伤害从 2% 调整为 2.5%</li>
-        <li>普雷利克斯变更：</li>
-        <ul>
-          <li>黑蜉蝣魔法抗性光环从 15% 调整为 12%</li>
-          <li>森林巨魔牧师治疗强化从 10% 调整为 8%</li>
-          <li>鹰身女妖风暴巫师连环闪电冷却时间从 4 调整为 6</li>
-          <li>鹰身女妖风暴巫师连环闪电伤害从 140 调整为 100</li>
-          <li>食人魔拳手食人魔挥击范围从 300 调整为 240</li>
-          <li>食人魔拳手食人魔挥击伤害从 15% 调整为 10%</li>
-          <li>萨特窃神者魔法回复光环从 10 调整至 8</li>
-          <li>萨特窃神者魔法灼烧从 100 基础伤害调整为 40</li>
-          <li>萨特苦难使者震荡波从 300 伤害调整为 240</li>
-          <li>萨特生命值回复从 10 调整为 8</li>
-          <li>萨特放逐者净化冷却时间从 8 调整为 12</li>
-          <li>番茄雷霆一击伤害从 300 调整为 240</li>
-          <li>番茄雷霆一击攻击速度与移动速度减缓从 55% 调整为 45%</li>
-        </ul>
-        <li>拉比克：拉比克冷却时间从 20/12/4 调整为 12/8/4</li>
-        <li>拉比克：升级技能窃取所需的点数从 0/5/15/25/35/45/55/65/75 调整为 0/7/14/21/28/35/42/49/56/63</li>
-        <li>旁观者：身体重选时不再提供折扣</li>
-        <li>恐怖分子炸弹定时器从 18/16/14/12/10 调整为 20/18/16/14/12</li>
-        <li>恐怖分子炸弹爆炸伤害从 90/80/70/60/50% 调整为 80/70/60/50/40%</li>
-      </ul>
-    </div>
+        <li>酒仙狂风小酒仙疾风步伤害从 0/20/40/60/80 调整为 0/60/120/180/240</li>
+        <li>酒仙狂风小酒仙的 3 个技能现都有最低 900 的施法距离（调整自 600）。这是为了防止其进入近战范围。如果你装备有施法距离更长的法术则此法术的施法距离将提高。此外，单位的攻击范围将在使用隐身法术时暂时提高至 900
+        </li>
+        <li>酒仙狂风小酒仙将不会再在敌人位于范围外时施放驱散</li>
+        <li>酒仙狂风小酒仙法术将立即消耗魔法值</li>
+        <li>酒仙烈火小酒仙献祭伤害从 0/25/50/75/100 调整为 0/32/64/96/128</li>
+        <li>酒仙大地小酒仙粉碎击攻击伤害从 0/20/40/60/80 调整为 0/25/50/75/100</li>
+        <li>CT 装备现会在于右下方悬浮在技能上时展示 AOE</li>
+        <li>CT 修复了高爆手雷有时无效的问题</li>
+        <li>克莱纳博士商店现会每购买 2 个法术后重选</li>
+        <li>克莱纳博士商店不再会在每回合结束后重选</li>
+        <li>添加了冰蛙神标签页以展示所选法术的工具提示</li>
+        <li>冰蛙：游戏开始时的法术选项从 5 个调整为 4 个</li>
+        <li>肉山 2 级法术抵抗将额外提供永久 50% 状态抗性</li>
+        <li>肉山 3 级猛击主要属性减免从 15% 调整为 25%</li>
+        <li>肉山 2 级重击额外将触发几率从 15% 提高至 20%</li>
+        <li>店主商店规模从 2/3/4 调整为 2/3/5</li>
+        <li>假期阿哈利姆触发几率从 5% 调整为 7%</li>
+    </ul>
   </div>
-</template>
+</div></template>
