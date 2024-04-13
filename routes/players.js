9260d89091854ba85eec2b389f3d061efeaa92da
VicFrank <matthewshattil@gmail.com>
2024-04-12 23:04:08 -0500

    removed console log

--- routes/players.js
+++ routes/players.js
@@ -881,7 +881,6 @@ router.get(
 
 router.post("/gaimin/check", auth.userAuth, async (req, res) => {
   try {
-    console.log(req.body);
     const steamIDs = req.body.steamIDs;
     const results = await players.checkGaimin(steamIDs);
     res.status(200).json(results);
