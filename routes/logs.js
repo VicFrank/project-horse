0b90c7fe75380beb26ede59cd4ab2789d646b791
Matthew Shattil <matthewshattil@gmail.com>
2024-12-09 14:50:12 -0600

    added test route

--- routes/logs.js
+++ routes/logs.js
@@ -68,4 +68,13 @@ router.post("/:gameID", auth.keyAuth, async (req, res) => {
   }
 });
 
+router.post("/test", auth.keyAuth, async (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.toString() });
+  }
+});
+
 module.exports = router;
